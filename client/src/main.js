3b53e36e6c2b61bd4565aa375e94709c21917c4f
Jurijs Koļesņikovs <jurijs_kolesnikovs@inbox.lv>
2020-01-28 20:55:01 +0700

    Added global components

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+/* Global components */
+import VContainer from '@/components/hoc/VContainer'
+import VRow from '@/components/hoc/VRow'
+import VShadowBox from '@/components/hoc/VShadowBox'
+
+Vue.component('v-container', VContainer)
+Vue.component('v-row', VRow)
+Vue.component('v-shadow-box', VShadowBox)
+
+import { GlobalDirectives } from '@/directives/directives';
+
+Vue.use(GlobalDirectives);
+
 import '@/assets/scss/main.scss'
 
 Vue.config.productionTip = false
