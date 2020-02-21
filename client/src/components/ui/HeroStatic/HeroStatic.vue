b5f7d359bfe31ac2a40ec88e896b5777243fb9bf
Yurii Kolesnikov <yk@vcv.ru>
2020-02-21 23:54:33 +0700

    Made static hero

---
+++ client/src/components/ui/HeroStatic/HeroStatic.vue
@@ -0,0 +1,13 @@
+<template>
+	<div 
+		class="HeroStatic"
+		style="background-image: url('/static/img/hero.jpg')">
+		<h1 class="HeroStatic__title">Travel Packages to Africa</h1>
+		<p class="HeroStatic__caption">
+			From desert nomads to gold miners, from jeep or on camel, up snow-capped mountains or on vast desert plateaus, Africa offers some of the deepest, richest, most diverse, most memorable experiences you will ever have.
+		</p>
+	</div>
+</template>
+<style lang="scss">
+@import "./HeroStatic";
+</style>
