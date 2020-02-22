b42fa0d409fdc6bd3f8f5f478b2705454eb6f723
Yurii Kolesnikov <yk@vcv.ru>
2020-02-22 23:02:44 +0700

    Made itinerary section in trip details

---
+++ client/src/components/ui/Itinerary/Itinerary.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="Itinerary">
+		<div class="Itinerary__day">Day {{ itinerary.day }}</div>
+		<p class="Itinerary__text">{{ itinerary.text }}</p>
+	</div>
+</template>
+<script>
+export default {
+	props: {
+		itinerary: Object
+	}
+}
+</script>
+<style lang="scss">
+@import "./Itinerary";
+</style>
