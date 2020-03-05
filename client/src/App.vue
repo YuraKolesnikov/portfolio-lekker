474fb8691ca51cd6717ee2f127956f7d96b7b079
Yurii Kolesnikov <yk@vcv.ru>
2020-03-05 22:36:15 +0700

    Made accommodation paginator

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <template>
 	<div id="app">
-		<Navbar />
+		<!-- <Navbar /> -->
 		<!-- <nav style="display: flex; width: 100%; justify-content: center">
 			<router-link to="/buttons">
 				<v-button option="filled">Buttons</v-button>
@@ -15,8 +15,17 @@
 				<v-button option="filled">Elements</v-button>
 			</router-link>
 		</nav> -->
-		<main>
-			<HeroStatic />
+		<main style="padding-top: 100px">
+			<v-container>
+				<v-row>
+					<div class="col-12">
+						<AccomodationPaginator 
+							:current-tab="currentTab"
+							@updateCurrentTab="updateCurrentTab" />
+					</div>
+				</v-row>
+			</v-container>
+			<!-- <HeroStatic /> -->
 			<div class="container">
 				<router-view />
 			</div>
@@ -27,12 +36,24 @@
 import VButton from '@/components/base/VButton/VButton'
 import HeroStatic from '@/components/ui/HeroStatic/HeroStatic'
 import Navbar from '@/components/TheNavbar/TheNavbar'
+import AccomodationPaginator from '@/components/ui/AccommodationPaginator/AccommodationPaginator'
 export default {
 	components: {
 		VButton,
 		HeroStatic,
-		Navbar
+		Navbar,
+		AccomodationPaginator
+	},
+	data() {
+		return {
+			currentTab: 5
+		}
+	},
+	methods: {
+		updateCurrentTab(id) {
+			console.log('updateCurrentTab catched!')
+			this.currentTab = id
+		}
 	}
-
 }
 </script>
---
+++ client/src/components/ui/AccommodationPaginator/AccommodationPaginator.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="AccommodationPaginator">
+		<ul class="AccommodationPaginator__paginator">
+			<li 
+				:key="tab"
+				v-for="tab in tabs"
+				@click="updateCurrentTab(tab)"
+				class="AccommodationPaginator__tab"
+				:class="{'AccommodationPaginator__tab--active': currentTab == tab}">
+				{{ tab }}
+				<svg>
+					<use xlink:href="static/icons/icons.svg#star"></use>
+				</svg>
+			</li>
+		</ul>
+		<div class="AccommodationPaginator__body">
+			<template v-for="page in accomodations">
+				<div class="AccommodationPaginator__page" :key="page.id" v-if="page.rating == currentTab">
+					<h4 class="AccommodationPaginator__page-title">{{ page.title }}</h4>
+					<p class="AccommodationPaginator__page-text">{{ page.description }}</p>
+					<img class="AccommodationPaginator__page-img" :src="page.img_path" alt="">
+				</div>
+			</template>
+		</div>
+	</div >
+</template>
+<script>
+import VStar from '@/components/base/VStar/VStar'
+export default {
+	props: {
+		currentTab: Number
+	},
+	data() {
+		return {
+			tabs: [5, 4, 3],
+			accomodations: [
+				{ id: 1, rating: 5, title: 'Kameelperd Laagte Zebula', description: 'Want something to blow your mind? Imagine a thousand flamingos standing at the side of Kenya’s Lake Nakuru. Or 15,000m2 of rolling savannah in the Serengeti. Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?  Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?', img_path: 'static/img/accomodation.png' },
+				{ id: 2, rating: 4, title: 'Kameelperd Laagte Zebula', description: 'Want something to blow your mind? Imagine a thousand flamingos standing at the side of Kenya’s Lake Nakuru. Or 15,000m2 of rolling savannah in the Serengeti. Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?  Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?', img_path: 'static/img/accomodation.png' },
+				{ id: 3, rating: 3, title: 'Kameelperd Laagte Zebula', description: 'Want something to blow your mind? Imagine a thousand flamingos standing at the side of Kenya’s Lake Nakuru. Or 15,000m2 of rolling savannah in the Serengeti. Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?  Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?', img_path: 'static/img/accomodation.png' }
+			]
+		}
+	},
+	components: {
+		VStar
+	},
+	methods: {
+		updateCurrentTab(id) {
+			this.$emit('updateCurrentTab', id)
+		}
+	}
+}
+</script>
+<style lang="scss">
+@import "./AccommodationPaginator";
+</style>
