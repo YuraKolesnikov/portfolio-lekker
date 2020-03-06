3b6496ebfd8c8b0d623a0fa037f39313f60e8918
Yurii Kolesnikov <yk@vcv.ru>
2020-03-06 22:53:55 +0700

    Accordion styling

--- client/src/components/ui/Accordion/Accordion.vue
+++ client/src/components/ui/Accordion/Accordion.vue
@@ -7,23 +7,20 @@
 				@click="toggle(option.id)">
 				{{ option.title }}
 			</div>
-			<transition name="accordion">
-				<div class="Accordion__body" v-if="show == option.id">
-					{{ option.text }}
-				</div>
-			</transition>
+			<div class="Accordion__body" :class="{'open': show == option.id}">
+				{{ option.text }}
+			</div>
 		</div>
 	</div>
 </template>
 <script>
 export default {
+	props: {
+		options: Array
+	},
 	data() {
 		return {
-			show: 1,
-			options: [
-				{ id: 1, title: 'Kenya', text: 'Want something to blow your mind? Imagine a thousand flamingos standing at the side of Kenya’s Lake Nakuru. Or 15,000m2 of rolling savannah in the Serengeti. Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?' },
-				{ id: 2, title: 'Tanzania', text: 'Want something to blow your mind? Imagine a thousand flamingos standing at the side of Kenya’s Lake Nakuru. Or 15,000m2 of rolling savannah in the Serengeti. Or why not explore the aromatic plantations of Zanzibar, famous for its coconut palm beaches and snorkelling in the Indian Ocean?' }
-			]
+			show: 1
 		}
 	},
 	methods: {
