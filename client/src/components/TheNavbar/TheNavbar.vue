f1592cbc991c8e6946be2a51665b0bc1a1267cec
Yurii Kolesnikov <yk@vcv.ru>
2020-04-07 21:31:46 +0700

    Removed junk code in Navbar

--- client/src/components/TheNavbar/TheNavbar.vue
+++ client/src/components/TheNavbar/TheNavbar.vue
@@ -4,12 +4,6 @@
 			<img src="static/img/logo.svg" alt="">
 		</a>
 		<div class="Navbar__main">
-			<v-select 
-				select-key="language"
-				:options="langs"
-				@selectOption="chooseLang"
-				:select-value="selectValue"
-				:computed-height='navbarHeight'></v-select>
 			<v-button option="outlined" @click="openModal">Request a callback</v-button>
 		</div>
 	</nav>
@@ -17,34 +11,14 @@
 <script>
 import VButton from '@/components/base/VButton/VButton'
 import VSelect from '@/components/base/VSelect/VSelect'
-import { EventBus } from '@/utils/EventBus'
 export default {
 	components: {
 		VButton,
 		VSelect
 	},
-	data() {
-		return {
-			langs: [
-				{ id: 'en', title: 'EN' },
-				{ id: 'ru', title: 'RU' }
-			],
-			selectValue: 'EN',
-			chosenLang: 'en'
-		}
-	},
-	computed: {
-		navbarHeight() {
-			return 70
-		}
-	},
 	methods: {
-		chooseLang(res) {
-			this.selectValue = res.option.title
-			this.chosenLang = res.option.id
-		},
 		openModal() {
-			EventBus.$emit('openModal')
+			this.$emit('openModal')
 		}
 	}
 }
